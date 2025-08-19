f1c88b7dc3afb9eac0276aaf90da07ff91ad8dcf
kazu <xingyanjintian65@gmail.com>
2025-08-19 09:33:16 +0900

    ユーザーの入力を受け取る処理を実装

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
-    println!("123");
+    println!("数当てゲーム開始！");
+    println!("予想した数を入力してね");
+
+    let mut guess = String::new();  //変数guessを作成 mutがあるのは可変
+
+    let apples = 5;                    // mutがないため不変
+
+    //入力を受け取る処理
+    io::stdin()
+    .read_line(&mut guess)
+        .expect("Failed to read line");     // 行の読み込みに失敗しました
+
+    println!("予想：{}", guess);
+
+
 }
