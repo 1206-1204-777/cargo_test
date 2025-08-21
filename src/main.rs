57c02511b628bb79270785685f26fa0a4426568e
kazu <xingyanjintian65@gmail.com>
2025-08-21 09:37:49 +0900

    乱数の生成機能を追加

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
+use rand::Rng;
 use std::io;
 fn main() {
     println!("数当てゲーム開始！");
     println!("予想した数を入力してね");
 
-    let mut guess = String::new();  //変数guessを作成 mutがあるのは可変
+    let mut guess = String::new(); //変数guessを作成 mutがあるのは可変
 
-    let apples = 5;                    // mutがないため不変
+    let apples = 5; // mutがないため不変
 
     //入力を受け取る処理
     io::stdin()
-    .read_line(&mut guess)
-        .expect("Failed to read line");     // 行の読み込みに失敗しました
+        .read_line(&mut guess)
+        .expect("Failed to read line"); // 行の読み込みに失敗しました
 
     println!("予想：{}", guess);
-    
-//数当てゲームで当てる数を生成
 
-
+    //数当てゲームで当てる数を生成
+    //乱数を生成
+    let select_number = rand::thread_rng().gen_range(1..101);
+    println!("The Seclet number:{}", select_number)
 }
