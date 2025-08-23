7ade05f0520cfc5a92832530add4e2e2fd4e1c93
kazu <xingyanjintian65@gmail.com>
2025-08-23 17:02:50 +0900

    数当て機能を追加

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use std::io;
+use std::{cmp::Ordering, io};
 fn main() {
     println!("数当てゲーム開始！");
     println!("予想した数を入力してね");
@@ -13,10 +13,19 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line"); // 行の読み込みに失敗しました
 
+    //変数guessを数値に変換
+    let guess: u32 = guess.trim().parse().expect("Please type a bumber!");
     println!("予想：{}", guess);
 
     //数当てゲームで当てる数を生成
     //乱数を生成
     let select_number = rand::thread_rng().gen_range(1..101);
-    println!("The Seclet number:{}", select_number)
+    println!("The Seclet number:{}", select_number);
+
+    //数を比較
+    match guess.cmp(&select_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => println!("Too match!"),
+        Ordering::Greater => println!("Too big!"),
+    }
 }
